e07446aaaf06bcaca08cf500c221201df5020e2b
Sonu Gupta <sonu72752@gmail.com>
2023-07-22 16:58:01 -0500

    updated readme & added lc20

--- LeetCode/121-BestTimeStock.java
+++ LeetCode/121-BestTimeStock.java
@@ -1,14 +1,15 @@
 package LeetCode;
 
+// Time: O(n)
 class Solution {
+
     public int maxProfit(int[] prices) {
         int maxProfit = 0;
         int minPrice = prices[0];
-        
-        for (int i = 1; i<prices.length; i++) {
+        for (int i = 1; i < prices.length; i++) {
             if (prices[i] > minPrice)
                 maxProfit = Math.max(maxProfit, (prices[i] - minPrice));
-            
+
             if (prices[i] < minPrice)
                 minPrice = prices[i];
         }
